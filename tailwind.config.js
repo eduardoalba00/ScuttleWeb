0132da290f4709c472f24bd9161f454f504360c3
Eduardo Alba <eduardoandresalba@gmail.com>
2024-04-01 02:16:53 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+import { fontFamily as _fontFamily } from "tailwindcss/defaultTheme";
+
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+    extend: {
+        fontFamily: {
+            sans: ["Inter var", ..._fontFamily.sans],
+        },
+    },
+};
+export const plugins = [];
